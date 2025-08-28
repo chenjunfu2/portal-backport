6739192a61e3f2ca0f33ac50fabfd4f5df913392
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-29 01:26:34 +0800

    删除不必要内容

--- src/main/java/chenjunfu2/portalbackport/Portalbackport.java
+++
@@ -1,9 +0,0 @@
-package chenjunfu2.portalbackport;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Portalbackport implements ModInitializer {
-	public static final String MOD_ID = "portal-backport";
-	@Override
-	public void onInitialize() {}
-}
